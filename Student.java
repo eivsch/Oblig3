52037185815a8c67e177b321bfa7deea4250a037
eivsch <eivindschulstad@gmail.com>
2013-10-23 19:16:05 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,13 +1,13 @@
 /*
 
-Programmering høst 2013
+Programmering hÃ¸st 2013
 Obligatorsik Oppgave 3
 Oppgave 2
 
 Gruppemedlemer:
 Eivind Schulstad	(s111111)
-Gretar Ævarsson		(s198586)
-Sigurd Hølleland	(s111111)
+Gretar Ã†varsson		(s198586)
+Sigurd HÃ¸lleland	(s111111)
 
 a) Programmer metoden private int erRegistrert( int nr ) { ... }.
 b) Programmer metoden public void innlevering( Oblig oppg ) { ... }.
@@ -51,8 +51,8 @@ public class Student
 
   private int erRegistrert( int nr )
   {
-     //< Undersøker om studenten allerede har registrert en oblig med
-     //  nummer lik den innkomne parameteren nr. Hvis så er tilfelle skal
+     //< UndersÃ¸ker om studenten allerede har registrert en oblig med
+     //  nummer lik den innkomne parameteren nr. Hvis sÃ¥ er tilfelle skal
      //  indeksen returneres. I motsatt fall returneres -1.  >
 
 		for( int i = 0; i < levert.length; i++ )
@@ -78,20 +78,26 @@ public class Student
   public void innlevering( Oblig oppg )
   {
     //< Hvis det tidligere er levert en oppgave med samme nummer som
-    //  den innkomne parameteren oppg har, og som IKKE har vært
+    //  den innkomne parameteren oppg har, og som IKKE har vÃ¦rt
     //  godkjent tidligere, skal den nye oppgaven erstatte den gamle.
-    //  Hvis obligen oppg ikke har vært levert tidligere skal den plasseres
-    //  på første ledige plass. >
+    //  Hvis obligen oppg ikke har vÃ¦rt levert tidligere skal den plasseres
+    //  pÃ¥ fÃ¸rste ledige plass. >
 
 		for( int i = 0; i < levert.length; i++ )
 		{
-			//Gretar commit:
-			if( erRegistrert( oppg.getObligNr() ) == -1 || levert[i] == null )
+            //eivind commit - gjorde om pÃ¥ hele, fungerer delvis, trenger flere if-tester
+			if( levert[i] == null)
+			{
 				levert[i] = oppg;
-			else if( erRegistrert( oppg.getObligNr() ) != -1 && !( levert[i].getGodkjent() ) )
+				break;
+			}
+
+			if (levert[i].getObligNr() == oppg.getObligNr() && !( levert[i].getGodkjent() ) )
+			{
 				levert[i] = oppg;
-
-
+				break;
+			}
+			//else if(levert[i].getObligNr ==
 			// old version:
 			/*
 			if( erRegistrert( oppg.getObligNr() ) != -1 && !( levert[i].getGodkjent() ) )
@@ -100,7 +106,6 @@ public class Student
 				levert[i] = oppg;
 			*/
 		}
-
 				/*
 					if( oppg.getObligNr() == levert[i].getObligNr() && !( levert[i].getGodkjent() ) )
 					levert[i] = oppg;
@@ -112,13 +117,15 @@ public class Student
 	public int ikkeGodkjent()
 	{
    	//< Returnerer antall oppgaver som enten ikke er godkjent eller ikke er
-    //  levert inn. ( Antall oppgaver som må være godkjent tilsvarer lengden
-    //  på arrayen. ) >
+    //  levert inn. ( Antall oppgaver som mÃ¥ vÃ¦re godkjent tilsvarer lengden
+    //  pÃ¥ arrayen. ) >
 		int antall = 0;
 
 		for( int i = 0; i < levert.length; i++ )
 		{
-			if( !( levert[i].getGodkjent() ) || levert[i] == null )
+			//Eivind commit - hindre nullpointerexception
+			if (levert[i] == null) break;
+			else if( !( levert[i].getGodkjent() ) || levert[i] == null )
 				antall++;
 		}
 
@@ -131,14 +138,17 @@ public class Student
   public String toString()
   {
     //< Returnerer studentens navn og klasse, samt opplysninger om hvilke
-    //  oppgaver som er godkjent, om vedkommende kan gå opp til eksamen, og
-    //  eventuelt hvor mange oppgaver som mangler for å kunne gå opp til eksamen.  >
+    //  oppgaver som er godkjent, om vedkommende kan gÃ¥ opp til eksamen, og
+    //  eventuelt hvor mange oppgaver som mangler for Ã¥ kunne gÃ¥ opp til eksamen.  >
 
     String output = navn + ", klasse: " + klasse + "\n";
 
     for( int i =  0; i < levert.length; i++ )
     {
-			if ( erRegistrert( levert[i].getObligNr() ) == -1 )
+		    //Eivind commit - hindre nullpointerexception
+		    if ( levert[i] == null) break;
+
+			else if ( erRegistrert( levert[i].getObligNr() ) == -1 )
 				output+= "Obligatorisk oppgave nr." + ( levert[i].getObligNr() ) + " er ikke levert\n";
 			else if( levert[i].getGodkjent() )
 				output+= "Obligatorisk oppgave nr." + ( levert[i].getObligNr() ) + " er godkjent\n";
@@ -149,11 +159,11 @@ public class Student
 
 	  if ( ikkeGodkjent() > 0)
 	  {
-	  	output+= navn + "kan ikke gå opp til eksamen\n";
-	  	output+= ikkeGodkjent() + " oppgaver mangler for å kunne gå opp til eksamen\n";
+	  	output+= navn + "kan ikke gÃ¥ opp til eksamen\n";
+	  	output+= ikkeGodkjent() + " oppgaver mangler for Ã¥ kunne gÃ¥ opp til eksamen\n";
 		}
 	  else
-    	output+= navn + "kan gå opp til eksamen\n";
+    	output+= navn + "kan gÃ¥ opp til eksamen\n";
 
     return output;
 
@@ -161,9 +171,9 @@ public class Student
 
 	  /*
 	  if ( ikkeGodkjent() > 0)
-	   output+= navn + "kan ikke gå opp til eksamen\n";
+	   output+= navn + "kan ikke gÃ¥ opp til eksamen\n";
 	  else
-    	output+= navn + "kan gå opp til eksamen\n";
+    	output+= navn + "kan gÃ¥ opp til eksamen\n";
 
     return output;
     */
